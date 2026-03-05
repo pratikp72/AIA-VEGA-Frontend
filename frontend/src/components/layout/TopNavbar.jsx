b1a3d6e0858e934b023df3d9dfc094be0427c05d
pratikp72 <pratikp72@gmail.com>
2026-03-05 18:19:53 +0530

    User var added

--- frontend/src/components/layout/TopNavbar.jsx
+++ frontend/src/components/layout/TopNavbar.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { Search, Bell, Users, BookOpen } from 'lucide-react';
+import { Search, Bell, Users, BookOpen, User  } from 'lucide-react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
