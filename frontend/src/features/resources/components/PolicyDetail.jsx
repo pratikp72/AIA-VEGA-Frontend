d39345dd7b69bf930ce2b502e2ad8f64fa31ffea
pratikp72 <pratikp72@gmail.com>
2026-03-23 11:48:28 +0530

    test commit

--- frontend/src/features/resources/components/PolicyDetail.jsx
+++ frontend/src/features/resources/components/PolicyDetail.jsx
@@ -118,6 +118,7 @@ export default function PolicyDetail({ item, fetchPolicyById }) {
             </div>
           </div>
         </SurfaceCard>
+        
 
         <div className="w-full gap-10 mt-6">
           <div className="">
