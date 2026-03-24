96fed6cee86bbf1c8c60739c8fb4667050017f67
vrunda310 <vrundam@zenithive.com>
2026-03-24 17:58:42 +0530

    bus stop link added in route page

--- frontend/src/features/routes/routesAPI.js
+++ frontend/src/features/routes/routesAPI.js
@@ -23,27 +23,28 @@ function normalizeRoute(route) {
   const rawSifts = attrs.bus_sifts ?? route?.bus_sifts ?? [];
   const arr = Array.isArray(rawSifts) ? rawSifts : rawSifts?.data ?? [];
   return {
-    id: attrs.route_id ?? route?.route_id ?? route?.id ?? null,
-    routeId: attrs.route_id ?? route?.route_id ?? null,
-    name: attrs.route_name ?? attrs.name ?? '',
+    id: attrs.stop_id ?? route?.stop_id ?? route?.id ?? null,
+    routeId: attrs.stop_id ?? route?.stop_id ?? null,
+    name: attrs.stop_name ?? attrs.name ?? '',
+    locationLink: attrs.bus_stop_location_link ?? route?.bus_stop_location_link ?? '',
     shifts: arr.map(normalizeSift),
   };
 }
 
 function normalizeUnitWithRoutes(u) {
   const attrs = u?.attributes ?? u ?? {};
-  const image = attrs.unit_img ?? u?.unit_img;
-  const rawRoutes = attrs.routes ?? u?.routes ?? [];
+  const image = attrs.route_img ?? u?.route_img ?? attrs.unit_img ?? u?.unit_img;
+  const rawRoutes = attrs.bus_stops ?? u?.bus_stops ?? attrs.routes ?? u?.routes ?? [];
   const routesArr = Array.isArray(rawRoutes) ? rawRoutes : rawRoutes?.data ?? [];
   return {
-    id: u?.id ?? u?.documentId ?? attrs.unit_id ?? null,
-    unitId: attrs.unit_id ?? null,
-    unitName: attrs.unit_name ?? attrs.name ?? '',
+    id: u?.id ?? u?.documentId ?? attrs.route_id ?? attrs.unit_id ?? null,
+    unitId: attrs.route_id ?? attrs.unit_id ?? null,
+    unitName: attrs.route_name ?? attrs.unit_name ?? attrs.name ?? '',
     address: attrs.address ?? '',
     contact: attrs.contact ?? '',
     siteManager: attrs.site_manager ?? '',
     hrManager: attrs.hr_manager ?? '',
-    mapLink: attrs.map_link ?? '',
+    mapLink: attrs.route_map_link ?? attrs.map_link ?? '',
     image: resolveMediaUrl(image),
     routes: routesArr.map(normalizeRoute),
   };
@@ -57,15 +58,15 @@ export const fetchLocationRoutes = async (locationId) => {
 
   const res = await apiService.get(API_ENDPOINTS.LOCATION.UNIT_LOCATION_BY_ID(locationId), {
     params: {
-      'populate[units][populate][unit_img]': true,
-      'populate[units][populate][routes][populate][bus_sifts]': true,
+      'populate[routes][populate][route_img]': true,
+      'populate[routes][populate][bus_stops][populate][bus_sifts]': true,
       'populate[company]': true,
       sort: 'name:asc',
     },
   });
 
   const data = res?.data ?? res;
-  const unitsRaw = data?.units ?? data?.attributes?.units ?? [];
+  const unitsRaw = data?.routes ?? data?.attributes?.routes ?? data?.units ?? data?.attributes?.units ?? [];
   const unitsArr = Array.isArray(unitsRaw) ? unitsRaw : unitsRaw?.data ?? [];
   const activeFlag = data?.attributes?.active ?? data?.active ?? true;
   if (activeFlag === false) return null;
