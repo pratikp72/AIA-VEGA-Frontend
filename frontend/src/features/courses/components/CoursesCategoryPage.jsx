e1936152fa4cb7fa6fd89295106b6843a3511944
pratikp72 <pratikp72@gmail.com>
2026-04-03 14:56:33 +0530

    image fix

--- frontend/src/features/courses/components/CoursesCategoryPage.jsx
+++ frontend/src/features/courses/components/CoursesCategoryPage.jsx
@@ -550,7 +550,7 @@ export default function CoursesCategoryPage({ category }) {
                       <img
                         src={course.image}
                         alt={course.title}
-                        className="w-full h-full object-contain rounded-3xl"
+                        className="w-full h-full object-cover rounded-3xl"
                         style={{ borderRadius: '18px' }}
                       />
                       <div className="absolute top-6 left-6 flex items-center gap-2 w-full pr-4">
