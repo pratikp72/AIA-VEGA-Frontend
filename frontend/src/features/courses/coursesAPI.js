8bcc4837951a6ef929e70deb4525d44f13d1268d
pratikp72 <pratikp72@gmail.com>
2026-04-06 19:33:59 +0530

    fixes

--- frontend/src/features/courses/coursesAPI.js
+++ frontend/src/features/courses/coursesAPI.js
@@ -130,7 +130,7 @@ function normalizeCourse(course) {
       : course.prerequisite_courses
         ? [course.prerequisite_courses]
         : [],
-    active: course.active !== false,
+    active: course.active !== 'unpublished',
   };
 }
 
@@ -157,7 +157,7 @@ async function fetchCourseDueDateMap() {
         'populate[departments]': true,
         'populate[individual_user]': true,
         'populate[work_locations]': true,
-        'filters[active][$eq]': true,
+        'filters[active][$eq]': 'published',
         'pagination[pageSize]': 1000,
         'pagination[page]': 1,
       },
@@ -357,7 +357,7 @@ export const fetchAllCourses = async () => {
   } while (page <= pageCount);
   const dueDateMap = await fetchCourseDueDateMap();
   return all
-    .filter(c => c.active !== false)
+    .filter(c => c.active !== 'unpublished')
     .map((course) => {
       const normalizedCourse = normalizeCourse(course);
       const assignedDueDate = dueDateMap[course?.id] || null;
